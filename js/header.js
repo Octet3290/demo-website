476f64fd8b63ef69a179971357eb42e3e18fca52
Rahul <rahul.yadav@yellowgap.com>
2025-12-30 12:52:45 +0530

    link

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 
-fetch("/components/header.html")
+fetch("./components/header.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("header").innerHTML = data;
